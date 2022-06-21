e6d274359e1c2a590faeedeeeea4426b61f301f1
M. Soban Amir <62153801+LoRdSoban@users.noreply.github.com>
2022-06-21 11:26:16 +0500

    Add project files.

---
+++ Login Page/Source.cpp
@@ -0,0 +1,476 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <string>
+#include <exception>
+#include <windows.h>
+#include "vaccine_system.h"
+using namespace std;
+
+
+void print_spaces(int space = 38)
+{
+	cout << "\n" << setfill(' ') << setw(space) << "";
+}
+
+HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+string banner = R"(
+                     __     __             _               ____            _        _ 
+                     \ \   / /_ _  ___ ___(_)_ __   ___   |  _ \ ___  _ __| |_ __ _| |
+                      \ \ / / _` |/ __/ __| | '_ \ / _ \  | |_) / _ \| '__| __/ _` | |
+                       \ V / (_| | (_| (__| | | | |  __/  |  __/ (_) | |  | || (_| | |
+                        \_/ \__,_|\___\___|_|_| |_|\___|  |_|   \___/|_|   \__\__,_|_|
+                                                                 
+
+)";
+
+
+//---------EXCEPTIONS---------------
+class invalid_username : public exception
+{
+public:
+	const char* what() const
+	{
+		return ("The entered username is not valid.");
+	}
+};
+
+class invalid_password;
+class wrong_password;
+class unregistered_username;
+//-------------------------------------------------------------
+
+void print_login(string username, string pass, int count)
+{
+	system("cls");
+	cout << banner;
+
+	print_spaces();
+	cout << setfill('-') << setw(10) << "";
+	cout << "LOGIN PAGE";
+	cout << setfill('-') << setw(10) << "\n\n";
+
+
+	print_spaces();
+	cout << "Username: ";
+	SetConsoleTextAttribute(h, 241);
+	cout << username;
+	SetConsoleTextAttribute(h, 240);
+
+	cout << "\n";
+	print_spaces();
+	cout << "Password: ";
+	for (int i = 0; i < count; i++)
+	{
+		SetConsoleTextAttribute(h, 241);
+		cout << "*";
+		SetConsoleTextAttribute(h, 240);
+	}
+}
+
+void login_page(string& username, string& pass)
+{
+	char pass_char;
+	int count = 0;
+	cout << banner;
+
+	print_spaces();
+	cout <<  setfill('-') << setw(10) << "";
+	cout << "LOGIN PAGE";
+	cout << setfill('-') << setw(10) << "\n\n" ;
+
+
+	print_spaces();
+	try
+	{
+		cout << "Username: ";
+		SetConsoleTextAttribute(h, 241);
+		cin >> username;
+		SetConsoleTextAttribute(h, 240);
+
+		if (username == "x")
+		{
+			throw invalid_username();
+		}
+
+		/*if (!is_valid_username(username))
+		{
+			throw invalid_username();
+		}*/
+
+		print_spaces();
+		cout << "Password: ";
+		pass_char = _getch();
+		while (pass_char != '\r')
+		{
+			cout << pass_char;
+			pass.push_back(pass_char);
+			count++;
+			print_login(username, pass, count);
+			pass_char = _getch();
+		}
+
+		/*if (!is_valid_password(pass))
+		{
+			throw invalid_password();
+		}*/
+	}
+	catch (const exception& e)
+	{
+		SetConsoleTextAttribute(h, 252);
+		print_spaces();
+		cout << "[!] Exception: " << e.what() << endl;
+		SetConsoleTextAttribute(h, 240);
+	}
+	
+
+	cout << "\n" << pass << endl;
+
+}
+#include <iostream>
+#include <string>
+using namespace std;
+
+class person
+{
+private:
+	string username, pass;
+	string firstname, lastname, email, CNIC, contactNumber;
+
+public:
+	person()
+	{
+		username = pass = firstname = lastname = email = contactNumber = CNIC = " ";
+		
+	}
+	person(string un, string fn, string ln, string em, string cn, string No) :username(un), firstname(fn), lastname(ln), email(em), CNIC(cn), contactNumber(No) {}
+
+	void input()
+	{
+		cout << "Enter First Name: ";
+		cin >> firstname;
+		cout << "Enter last Name: ";
+		cin >> lastname;
+		cout << "Enter Email Address: ";
+		cin >> email;
+		cout << "Enter Contact Number: ";
+		cin >> contactNumber;
+		cout << "Enter CNIC: ";
+		cin >> CNIC;
+	}
+
+	void output()
+	{
+		cout << "First Name: " << firstname << endl;
+		cout << "last Name: " << lastname << endl;
+		cout << "Email Address: " << email << endl;
+		cout << "Contact Number: " << contactNumber << endl;
+		cout << "CNIC: " << CNIC << endl;
+	}
+
+	bool compare_username(string un)
+	{
+		if (username == un)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	virtual void input_login_details() = 0; // inout username and password
+};
+
+
+class admin : public person{};
+class fdo : public person {};
+class doctor : public person {};
+class government_official : public person {};
+
+class super_admin : public person
+{
+private:
+	static super_admin* self_instance;
+
+	int admin_count, fdo_count, doctor_count, gov_off_count;
+
+	admin* admin_list;
+	fdo* fdo_list;
+	doctor* doctor_list;
+	government_official* gov_off_list;
+
+	super_admin(string un, string fn, string ln, string em, string cn, string No) : person(un, fn, ln, em, No, cn)
+	{
+		// read admin, fdo, doctor, government officials from file
+	}
+
+	int find_admin_index(string un)
+	{
+		for (int i = 0; i < admin_count; i++)
+		{
+			if (admin_list[i].compare_username(un))
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	int find_fdo_index(string un)
+	{
+		for (int i = 0; i < fdo_count; i++)
+		{
+			if (fdo_list[i].compare_username(un))
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	int find_doctor_index(string un)
+	{
+		for (int i = 0; i < doctor_count; i++)
+		{
+			if (doctor_list[i].compare_username(un))
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	int find_gov_off_index(string un)
+	{
+		for (int i = 0; i < gov_off_count; i++)
+		{
+			if (gov_off_list[i].compare_username(un))
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+public:
+
+	static super_admin* getInstance(string un, string fn, string ln, string em, string No, string cn)
+	{
+		if (self_instance == nullptr)
+			self_instance = new super_admin(un, fn, ln, em, No, cn);
+
+		return self_instance;
+	}
+
+	void create_admin()
+	{
+		
+		expand<admin>(admin_list, admin_count);
+
+		admin_list[admin_count - 1].input_login_details();
+		admin_list[admin_count - 1].input();
+
+		Filing::add_new_admin(admin_list[admin_count - 1]);
+	}
+
+	void delete_admin()
+	{
+		if (admin_count > 0)
+		{
+			int i;
+			string un;
+			cout << "Enter the username of admin: ";
+			cin >> un;
+
+			i = find_admin_index(un);
+
+			if (i != -1)
+			{
+				del<admin>(admin_list, admin_count, i);
+				admin_count--;
+				Filing::store_deleted_admins(un, admin_list, admin_count);
+			}
+			else
+			{
+				//error messages display karwa
+			}
+		}
+		else
+		{
+			print_error("\n\n[!]ERROR: There is no admin to delete.\n\n");
+		}
+	}
+
+	void update_admin()
+	{
+		if (admin_count > 0)
+		{
+			int i;
+			string un;
+			cout << "Enter the username of admin: ";
+			cin >> un;
+
+			i = find_admin_index(un);
+
+			if (i != -1)
+			{
+				admin_list[i].input_login_details();
+				admin_list[i].input();
+
+				Filing::store_updated_admins(un, admin_list, admin_count);
+			}
+		}
+		else
+		{
+			print_error("\n\n[!]ERROR: There is no admin to update.\n\n");
+		}
+			
+	}
+
+
+	void input_login_details(){}
+
+
+	
+
+};
+
+super_admin* super_admin::self_instance = nullptr; // initialization
+
+void super_admin_page(string username)
+{
+	super_admin* SA = Filing::login_super_admin(username);
+}
+
+void login_process(string username, string pass)
+{
+	string modes[5] = { "SA", "AD", "DC", "FD", "GO" };
+	string mode = username.substr(0, 2);
+	try
+	{
+		if (mode == modes[0]) // Super Admin
+		{
+			if (Filing::find_super_admin(username))
+			{
+				if (Filing::is_valid_login_super_admin(username, pass))
+				{
+					super_admin_page(username);
+				}
+				else
+				{
+					//throw wrong_password();
+				}
+			}
+			else
+			{
+				//throw unregistered_username();
+			}
+		}
+		else if (mode == modes[1]) // Admin
+		{
+			if (Filing::find_admin(username))
+			{
+				if (Filing::is_valid_login_admin(username, pass))
+				{
+					admin_page(username);
+				}
+				else
+				{
+					//throw wrong_password();
+				}
+			}
+			else
+			{
+				throw unregistered_username();
+			}
+		}
+		else if (mode == modes[2]) // Doctor
+		{
+			if (Filing::find_doctor(username))
+			{
+				if (Filing::is_valid_login_doctor(username, pass))
+				{
+					doctor_page(username);
+				}
+				else
+				{
+					throw wrong_password();
+				}
+			}
+			else
+			{
+				//throw unregistered_username();
+			}
+		}
+		else if (mode == modes[3]) // Front Desk Officer
+		{
+			if (Filing::find_fdo(username))
+			{
+				if (Filing::is_valid_login_fdo(username, pass))
+				{
+					fdo_page(username);
+				}
+				else
+				{
+					throw wrong_password();
+				}
+			}
+			else
+			{
+				//throw unregistered_username();
+			}
+		}
+		else if (mode == modes[4]) // Government Offcial
+		{
+			if (Filing::find_gov_off(username))
+			{
+				if (Filing::is_valid_login_gov_off(username, pass))
+				{
+					//gov_off_page(username);
+				}
+				else
+				{
+					//throw wrong_password();
+				}
+			}
+			else
+			{
+				//throw unregistered_username();
+			}
+		}
+	}
+	catch (const exception& e)
+	{
+		SetConsoleTextAttribute(h, 252);
+		print_spaces();
+		cout << "[!] Exception: " << e.what() << endl;
+		SetConsoleTextAttribute(h, 240);
+	}
+		
+}
+
+int main()
+{
+	system("Color F0");
+	string a, b;
+
+	login_page(a, b);
+
+	login_process(a, b);
+
+	
+
+
+
+	
+
+	system("pause");
+	return 0;
+}
---
+++ Login Page/vaccine_system.h
@@ -0,0 +1,466 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+string FILE_PATH = "";
+
+template<typename T>
+void expand(T*& ptr, int& size)
+{
+    T* temp = new T[size];
+    for (int i = 0; i < size; i++)
+    {
+        temp[i] = ptr[i];
+    }
+
+    size++;
+
+    delete[] ptr;
+    ptr = new T[size];
+
+    for (int i = 0; i < size-1; i++)
+    {
+        ptr[i] = temp[i];
+    }
+}
+
+template <typename T>
+void del(T*& ptr, int size, int index)
+{
+    T* temp = new T[size - 1];
+    int count = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (i != index)
+        {
+            temp[count++] = ptr[i];
+        }
+    }
+
+    delete[] ptr;
+
+    ptr = new T[size - 1];
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        ptr[i] = temp[i];
+    }
+}
+
+string encrypt(string username, string pass)
+{
+    int key = 0;
+
+    // calculates the key used for encryption
+    for (int i = 2; i < 7; i++)
+    {
+        key += username[i] - 48; // converting char number into integar
+    }
+
+    cout << "key " << key << endl;
+
+    // coverts the entered password into encrypted one to compare with the original
+    for (int i = 0; i < pass.size(); i++)
+    {
+
+        pass[i] += key;
+        cout << int(pass[i]) << endl;
+
+        if (int(pass[i]) < 0) // makes sure ASCII doesn't go in negative value
+        {
+            pass[i] *= -1;
+        }
+
+        if (int(pass[i]) == 127) // 127 is the delete character; causes problems in comparing the passwords
+        {
+            pass[i] -= 1;
+        }
+        cout << int(pass[i]) << endl;
+    }
+
+    return pass;
+}
+
+void print_error(string output)
+{
+    SetConsoleTextAttribute(h, 252);
+    cout << output;
+    SetConsoleTextAttribute(h, 240);
+}
+
+class Filing
+{
+public:
+    static bool find_super_admin(string username)
+    {
+        ifstream login_file(FILE_PATH + "super_admin_login_details.txt");
+        string o_username;
+
+        login_file >> o_username; // reads original username from the file
+
+
+        if (username == o_username)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+	static bool is_valid_login_super_admin(string username, string pass)
+    {
+        ifstream login_file(FILE_PATH + "super_admin_login_details.txt");
+        string o_username, o_pass;
+        int key = 0;
+
+        login_file >> o_username >> o_pass; // reads original username and password from the file
+
+        pass = encrypt(username, pass);
+
+        if (username == o_username && pass == o_pass)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    static super_admin* login_super_admin(string username)
+    {
+        ifstream details_file(FILE_PATH + "super_admin_details.csv");
+        int count = 0;
+        string line, word;
+        string details[6];
+        //string o_username, fn, ln, em, No, cn;
+
+        getline(details_file, line);
+        stringstream str(line);
+
+        while (getline(str, word, ','))
+        {
+            details[count++] = word;
+        }
+
+        if (details[0] == username)
+        {
+            return super_admin::getInstance(details[0], details[1], details[2], details[3], details[4], details[5]);
+        }
+    }
+
+	static bool find_admin(string username)
+    {
+        ifstream login_file(FILE_PATH + "admin_login_details.txt");
+
+        bool found = false;
+        string o_username, o_pass;
+
+         // reads original username from the file
+        while (!login_file.eof())
+        {
+            login_file >> o_username >> o_pass;
+            if (username == o_username)
+            {
+                found = true;
+                break;
+            }
+            
+        }
+        login_file.close();
+
+        return found;
+    }
+    static bool is_valid_login_admin(string username, string pass)
+    {
+        ifstream login_file(FILE_PATH + "admin_login_details.txt");
+
+        bool logined = false;
+        string o_username, o_pass;
+
+         // reads original username from the file
+        while (!login_file.eof())
+        {
+            login_file >> o_username >> o_pass;
+            if (username == o_username)
+            {
+                pass = encrypt(username, pass);
+
+                if (pass == o_pass)
+                {
+                    logined = true;
+                    break;
+                }
+            }
+                
+        }
+        login_file.close();
+
+        return logined;
+    }
+    static void add_new_admin(admin& obj)
+    {
+        ofstream details_file(FILE_PATH + "admin_details.csv", ios::app);
+        ofstream login_file(FILE_PATH + "admin_login_details.txt", ios::app);
+
+        if (!details_file || !login_file)
+        {
+            print_error("\n\nERROR IN OPENING THE FILE: admin_details.csv or admin_login_details.txt\n\n");
+        }
+        else                                 
+        {                                    
+            details_file << obj.file_details();
+            login_file << obj.file_logindetails();
+                                             
+            details_file.close();
+            login_file.close();
+        }
+    }
+    static void store_deleted_admins(string username, admin* arr, int size)
+    {
+        ofstream details_file(FILE_PATH + "admin_details.csv");
+        
+
+        if (!details_file)
+        {
+            print_error("\n\nERROR IN OPENING THE FILE: admin_details.csv or admin_login_details.txt\n\n");
+        }
+        else
+        {
+            details_file << obj.file_details();
+
+            details_file.close();
+        }
+
+        vector<string> o_username, o_pass;
+        string un, pass;
+        int count = 0;;
+
+        ifstream login_input_file(FILE_PATH + "admin_login_details.txt");
+
+        while (!login_input_file.eof())
+        {
+            login_input_file >> un >> pass;
+            o_username.push_back(un);
+            o_pass.push_back(pass);
+            count++;
+        }
+        login_input_file.close();
+
+        ofstream login_output_file(FILE_PATH + "admin_login_details.txt");
+
+        for (int i = 0; i < count; i++)
+        {
+            if (username != o_username[i])
+            {
+                login_output_file << o_username[i] << " " << o_pass[i] << endl;
+            }
+        }
+
+        login_output_file.close();
+    }
+    static void store_updated_admins(string username, admin* arr, int size)
+    {
+        ofstream details_file(FILE_PATH + "admin_details.csv");
+
+
+        if (!details_file)
+        {
+            print_error("\n\nERROR IN OPENING THE FILE: admin_details.csv or admin_login_details.txt\n\n");
+        }
+        else
+        {
+            details_file << obj.file_details();
+
+            details_file.close();
+        }
+
+        vector<string> o_username, o_pass;
+        string un, pass;
+        int count = 0;;
+
+        ifstream login_input_file(FILE_PATH + "admin_login_details.txt");
+
+        while (!login_input_file.eof())
+        {
+            login_input_file >> un >> pass;
+            o_username.push_back(un);
+            o_pass.push_back(pass);
+            count++;
+        }
+        login_input_file.close();
+
+        ofstream login_output_file(FILE_PATH + "admin_login_details.txt");
+
+        for (int i = 0; i < count; i++)
+        {
+            if (username == o_username[i])
+            {
+                login_output_file << obj.file_logindetails();
+            }
+            else
+            {
+                login_output_file << o_username[i] << " " << o_pass[i] << endl;
+            }
+        }
+
+        login_output_file.close();
+    }
+  
+    static bool find_doctor(string username)
+    {
+        ifstream login_file(FILE_PATH + "doctor_login_details.txt");
+
+        bool found = false;
+        string o_username, o_pass;
+
+
+        while (!login_file.eof())
+        {
+            login_file >> o_username >> o_pass;  // reads original username from the file
+            if (username == o_username)
+            {
+                found = true;
+                break;
+            }
+            login_file >> o_username >> o_pass;
+        }
+        login_file.close();
+
+        return found;
+    }
+    static bool is_valid_login_doctor(string username, string pass)
+    {
+        ifstream login_file(FILE_PATH + "doctor_login_details.txt");
+
+        bool logined = false;
+        string o_username, o_pass;
+
+        
+        while (!login_file.eof())
+        {
+            login_file >> o_username >> o_pass; // reads original username from the file
+            if (username == o_username)
+            {
+                pass = encrypt(username, pass);
+
+                if (pass == o_pass)
+                {
+                    logined = true;
+                    break;
+                }
+            }
+
+        }
+        login_file.close();
+
+        return logined;
+    }
+
+	static bool find_fdo(string username)
+    {
+        ifstream login_file(FILE_PATH + "fdo_login_details.txt");
+
+        bool found = false;
+        string o_username, o_pass;
+
+
+        while (!login_file.eof())
+        {
+            login_file >> o_username >> o_pass;  // reads original username from the file
+            if (username == o_username)
+            {
+                found = true;
+                break;
+            }
+            login_file >> o_username >> o_pass;
+        }
+        login_file.close();
+
+        return found;
+    }
+	static bool is_valid_login_fdo(string username, string pass)
+    {
+        ifstream login_file(FILE_PATH + "fdo_login_details.txt");
+
+        bool logined = false;
+        string o_username, o_pass;
+
+
+        while (!login_file.eof())
+        {
+            login_file >> o_username >> o_pass; // reads original username from the file
+            if (username == o_username)
+            {
+                pass = encrypt(username, pass);
+
+                if (pass == o_pass)
+                {
+                    logined = true;
+                    break;
+                }
+            }
+        }
+        login_file.close();
+
+        return logined;
+    }
+
+	static bool find_gov_off(string username)
+    {
+        ifstream login_file(FILE_PATH + "gov_off_details.txt");
+
+        bool found = false;
+        string o_username, o_pass;
+
+
+        while (!login_file.eof())
+        {
+            login_file >> o_username >> o_pass;  // reads original username from the file
+            if (username == o_username)
+            {
+                found = true;
+                break;
+            }
+            login_file >> o_username >> o_pass;
+        }
+        login_file.close();
+
+        return found;
+    }
+	static bool is_valid_login_gov_off(string username, string pass)
+    {
+        ifstream login_file(FILE_PATH + "gov_off_login_details.txt");
+
+        bool logined = false;
+        string o_username, o_pass;
+
+
+        while (!login_file.eof())
+        {
+            login_file >> o_username >> o_pass; // reads original username from the file
+            if (username == o_username)
+            {
+                pass = encrypt(username, pass);
+
+                if (pass == o_pass)
+                {
+                    logined = true;
+                    break;
+                }
+            }
+        }
+        login_file.close();
+
+        return logined;
+    }
+
+	
+};
